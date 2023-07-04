3a10e357b26aaac84d06ecbf937b7e26b42617c3
Riku Choy <rikuchoy@gmail.com>
2023-07-03 19:00:25 -0700

    set up fields and rules for Tag Model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true 
+    },
+    product_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'product', 
+        key: 'id' // references the id key of the product model
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'tag',
+        key: 'id' // references the id key of the tag model
+      }
+    }
   },
   {
     sequelize,
